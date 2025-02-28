ed0d33c72c27b0dc93f294b61cecf8d5e752abd5
oldfeel <hyt5926@163.com>
2025-03-01 00:13:34 +0800

    add main files toolbar

--- syncthing-android/app/src/main/java/tech/shupi/mydata/base/BaseFragment.kt
+++ syncthing-android/app/src/main/java/tech/shupi/mydata/base/BaseFragment.kt
@@ -1,6 +1,24 @@
 package tech.shupi.mydata.base
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.nutomic.syncthingandroid.R
 
 open class BaseFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar: Toolbar? = view.findViewById(R.id.toolbar)
+
+        if (toolbar != null) {
+            (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        }
+    }
+
+    fun setTitle(title: String) {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = title
+    }
 }
--- syncthing-android/app/src/main/java/tech/shupi/mydata/fragments/MainFilesFragment.kt
+++ syncthing-android/app/src/main/java/tech/shupi/mydata/fragments/MainFilesFragment.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import com.nutomic.syncthingandroid.databinding.FragmentMainFilesBinding
 import com.nutomic.syncthingandroid.databinding.FragmentMainTasksBinding
 import tech.shupi.mydata.base.BaseFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.nutomic.syncthingandroid.R
 
 class MainFilesFragment : BaseFragment() {
     private lateinit var binding: FragmentMainFilesBinding
@@ -18,5 +21,11 @@ class MainFilesFragment : BaseFragment() {
     ): View {
         binding = FragmentMainFilesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setTitle("Main Files")
     }
 }
